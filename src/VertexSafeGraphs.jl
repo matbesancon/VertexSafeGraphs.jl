3909061266afb3cb724c90beada52ef8d9b50fe0
Mathieu Besançon <mathieu.besancon@gmail.com>
2019-05-29 18:07:22 +0200

    added tests & code

--- src/VertexSafeGraphs.jl
+++ src/VertexSafeGraphs.jl
@@ -1,5 +1,51 @@
 module VertexSafeGraphs
 
-greet() = print("Hello World!")
+import LightGraphs
+const LG = LightGraphs
+
+export VSafeGraph
+
+struct VSafeGraph{T, G<:LG.AbstractGraph{T}, V<:AbstractVector{Int}} <: LG.AbstractGraph{T}
+    g::G
+    deleted_vertices::V
+    VSafeGraph(g::G, v::V) where {T, G<:LG.AbstractGraph{T}, V<:AbstractVector{Int}} = new{T, G, V}(g, v)
+end
+
+VSafeGraph(g::G) where {G<:LG.AbstractGraph} = VSafeGraph(g, Vector{Int}())
+VSafeGraph(nv::Integer) = VSafeGraph(LG.SimpleGraph(nv))
+
+LG.edges(g::VSafeGraph) = LG.edges(g.g)
+LG.edgetype(g::VSafeGraph) = LG.edgetype(g.g)
+LG.is_directed(g::VSafeGraph) = LG.is_directed(g.g)
+LG.ne(g::VSafeGraph) = LG.ne(g.g)
+LG.nv(g::VSafeGraph) = LG.nv(g.g) - length(g.deleted_vertices)
+LG.vertices(g::VSafeGraph) = (v for v in LG.vertices(g.g) if !(v in g.deleted_vertices))
+
+LG.outneighbors(g::VSafeGraph, v) = LG.outneighbors(g.g, v)
+LG.inneighbors(g::VSafeGraph, v) = LG.inneighbors(g.g, v)
+LG.has_vertex(g::VSafeGraph, v) = LG.has_vertex(g.g, v) && !(v in g.deleted_vertices)
+
+LG.has_edge(g::VSafeGraph, e) = LG.has_edge(g.g, e)
+
+LG.add_edge!(g::VSafeGraph, v1, v2) = LG.add_edge!(g.g, v1, v2)
+LG.add_vertex!(g::VSafeGraph, v) = LG.add_vertex!(g.g, v)
+
+LG.rem_edge!(g::VSafeGraph, v1, v2) = LG.rem_edge!(g.g, v1, v2)
+
+Base.copy(g::VSafeGraph) = VSafeGraph(copy(g.g), copy(g.deleted_vertices))
+
+function LG.rem_vertex!(g::VSafeGraph, v1)
+    if !LG.has_vertex(g, v1) || v1 in g.deleted_vertices
+        return false
+    end
+    for v2 in LG.outneighbors(g, v1)
+        LG.rem_edge!(g, v1, v2)
+    end
+    for v2 in LG.inneighbors(g, v1)
+        LG.rem_edge!(g, v2, v1)
+    end
+    push!(g.deleted_vertices, v1)
+    return true
+end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,47 @@
 using VertexSafeGraphs
-using Test
 
-@testset "VertexSafeGraphs.jl" begin
-    # Write your own tests here.
+using Test
+import Random
+
+import LightGraphs
+const LG = LightGraphs
+
+@testset "Graph construction and basic interface" begin
+    nv = 20
+    g1 = VSafeGraph(nv)
+    @test LG.nv(g1) == nv
+    @test LG.nv(g1.g) == nv
+
+    g2_inner = LG.CompleteGraph(nv)
+    g2 = VSafeGraph(g2_inner)
+    @test LG.nv(g2) == LG.nv(g2_inner)
+    @test LG.ne(g2) == LG.ne(g2_inner)
+
+    @test all(sort(collect(LG.vertices(g2))) .== sort(collect(LG.vertices(g2_inner))))
+
+    g3 = VSafeGraph(LG.CompleteDiGraph(30))
+    @test LG.is_directed(g3)
+    @test !LG.is_directed(g2)
+end
+
+@testset "Vertex deletion" begin
+    Random.seed!(33)
+    nv = 45
+    inner = LG.CompleteGraph(nv)
+    g = VSafeGraph(inner)
+    @test LG.ne(inner) == LG.ne(g)
+    @test LG.nv(inner) == LG.nv(g)
+    nrm = 0
+    for _ in 1:15
+        removed_ok = LG.rem_vertex!(g, rand(1:nv))
+        if !removed_ok
+            continue
+        end
+        nrm += 1
+        @test LG.nv(inner) == nv
+        @test LG.nv(g) == nv - nrm
+        @test length(g.deleted_vertices) == nrm
+
+        @test LG.ne(inner) == LG.ne(g)
+    end
 end
