fe7a8c3034adca21b3eccae1948cff1c1d12d1f1
Pankaj Mishra <pankajmishra1511@gmail.com>
2019-06-02 20:58:51 +0530

    added tests for modified functions
    Added tests for edge addition and neighbor set functions. test for LG algorithm still fails.

--- test/runtests.jl
+++ test/runtests.jl
@@ -46,6 +46,67 @@ end
     end
 end
 
+
+@testset "Edge addition" begin
+    Random.seed!(45)
+    nv = 10
+    inner = LG.CompleteGraph(nv)
+    g = VSafeGraph(inner)
+    nea = 0         #number of edges added
+    nrv = 0         #number of removed vertices
+    neo = LG.ne(g)  #original number of edges
+    for _ in 1:5
+        removed_ok = LG.rem_vertex!(g, rand(1:nv))
+        if !removed_ok
+            continue
+        end
+        nrv += 1
+    end
+
+    ne = length(LG.edges(g))   #new number of edges
+    for _ in 1:10
+        v1 = rand(1:nv)
+        v2 = rand(1:nv)
+        while v2 == v1
+            v2 = rand(1:nv)
+        end
+        added_ok = LG.add_edge!(g, v1, v2)
+        if added_ok
+            nea += 1
+        end
+
+        @test LG.ne(g) == ne + nea
+	@test LG.nv(g) == nv - nrv
+    end
+end
+
+@testset "Neighbor sets" begin
+    Random.seed!(99)
+    nv = 10
+    inner = LG.CompleteGraph(nv)
+    g = VSafeGraph(inner)
+    nrv = 0
+    for _ in 1:5
+        rv = rand(1:nv)
+        tv = rand(1:nv)
+        while !LG.has_vertex(g, tv)
+            tv = rand(1:nv)
+        end
+	removed_ok = LG.rem_vertex!(g, rv)
+        if !removed_ok
+            continue
+        end
+        nrv += 1
+        @test_throws ArgumentError LG.inneighbors(g, rv)
+        @test_throws ArgumentError LG.outneighbors(g, rv)
+        @test !(rv in LG.outneighbors(g, tv))
+        @test !(rv in LG.inneighbors(g, tv))
+        @test LG.nv(g) == nv - nrv
+    end
+end
+
+
+
 @testset "Generic LG algorithms work" begin
     nv = 45
     inner = LG.CompleteGraph(nv)
